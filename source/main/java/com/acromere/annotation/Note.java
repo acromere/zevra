514104ebe437b32dfe9d3f87a953c5ae86621c4b
Mark Soderquist <mark@soderquist.net>
2026-05-22 12:28:28 -0600

    Expand `@Target` annotation in `Note.java` to include additional `ElementType` values for broader applicability.

--- source/main/java/com/acromere/annotation/Note.java
+++ source/main/java/com/acromere/annotation/Note.java
@@ -2,7 +2,7 @@ package com.acromere.annotation;
 
 import java.lang.annotation.*;
 
-@Target( { ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.CONSTRUCTOR } )
+@Target( { ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE, ElementType.CONSTRUCTOR, ElementType.ANNOTATION_TYPE, ElementType.PACKAGE, ElementType.MODULE, ElementType.RECORD_COMPONENT, ElementType.TYPE_PARAMETER, ElementType.TYPE_USE } )
 @Retention( RetentionPolicy.SOURCE )
 @Repeatable( Notes.class )
 @Documented
