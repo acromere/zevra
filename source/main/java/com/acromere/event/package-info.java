accf3e9a0f713089eb5013aa0bcd19d31f112bc0
Mark Soderquist <mark@soderquist.net>
2026-09-02 20:08:59 -0600

    Update JavaDoc in com.acromere.event package-info.java documentation
    - Added a missing comma in the JavaDoc for improved grammar and clarity.
    - Enhanced overall readability of the package description.

--- source/main/java/com/acromere/event/package-info.java
+++ source/main/java/com/acromere/event/package-info.java
@@ -2,7 +2,7 @@
  * A simple, yet powerful, general event system. Events for any kind of activity
  * can be defined and distributed with the classes in this package. Events are
  * defined by subclassing the {@link com.acromere.event.Event Event} class,
- * defining {@link com.acromere.event.EventType EventTypes} for that class
+ * defining {@link com.acromere.event.EventType EventTypes} for that class,
  * and distributing them with the {@link com.acromere.event.EventHub EventHub}
  * class. {@link com.acromere.event.EventHandler Handlers} can be registered to
  * handle only the event types it can handle and use of lambdas as the handler
